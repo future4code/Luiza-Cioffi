3728c83c5b7e502b7c086e48be7aab7d757bab67
Luiza Cioffi <luizagac@gmail.com>
2021-10-06 20:51:51 -0300

    exercicios-arrays

---
+++ modulo1/string-arrays/index.js
@@ -0,0 +1,43 @@
+// Exercício de interpretação de código
+// 1.
+/*a. null/undefined
+b. null
+c. 11
+d. 3
+e. 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13
+f. 9
+*/
+
+//2.
+/* SUBI NUM ÔNIBUS EM MIRROCOS
+27
+*/
+
+
+// Exercícios de escrita de código
+//1.
+/*const nomeDoUsuario = prompt("Olá, qual o seu nome?")
+const emailDoUsuario = prompt("E qual o seu e-mail?")
+
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`)
+//ou
+console.log("O e-mail " + emailDoUsuario + " foi cadastrado com sucesso. Seja bem-vinda(o), " + nomeDoUsuario + "!")
+*/
+
+//2.
+//a.
+let comidaBoa = ["chocolate", "carne moída", "pudim", "risoto", "pizza"]
+console.log(comidaBoa)
+//b.
+/*console.log("Essas são as minhas comidas preferidas:")
+console.log(comidaBoa[0])
+console.log(comidaBoa[1])
+console.log(comidaBoa[2])
+console.log(comidaBoa[3])
+console.log(comidaBoa[4])*/
+//c.
+/*let  comidaUsuario = prompt("Qual é a sua comida preferida?")
+let comidaBoaDois = comidaBoa.replaceAll("carne moída", comidaUsuario)
+*/
+
+//
