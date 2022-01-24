c2b156756e509a7cc8ebd1b05b6d8ff272dd4924
Luiza Cioffi <luizagac@gmail.com>
2022-01-24 19:19:30 -0300

    exercícios de intro-ts resolvidos

---
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,8 @@
+function imprimeTresCoresFavoritas() {
+    const cor1:string = "amarelo"
+    const cor2:string = "vermelho"
+    const cor3:string = "azul"
+    console.log([cor1, cor2, cor3])
+}
+
+imprimeTresCoresFavoritas()
