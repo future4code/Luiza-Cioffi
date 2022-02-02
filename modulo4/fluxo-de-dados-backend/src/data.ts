db9d053b98985b30c8e6552c37d6216699b194c4
Luiza Cioffi <luizagac@gmail.com>
2022-02-02 20:15:18 -0300

    fluxo de dados backend - início

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,34 @@
+type Objects = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const objects:Objects[] = [
+    {
+        id: "1",
+        name: "Espelho de couro com alça",
+        price: 369,
+    },
+    {
+        id: "2",
+        name: "Aparador de grama elétrico",
+        price: 160,
+    },
+    {
+        id: "3",
+        name: "Tênis sb Chron 2 canvas",
+        price: 400,
+    },
+    {
+        id: "4",
+        name: "Cadeirinha infantil para carro",
+        price: 200,
+    },
+    {
+        id: "5",
+        name: "Máquina de café espresso",
+        price: 450,
+    }
+
+]
