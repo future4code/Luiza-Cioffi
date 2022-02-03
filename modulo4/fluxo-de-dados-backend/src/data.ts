5f3d215094e35cb66af37332c61437fb5e19c1c1
Luiza Cioffi <luizagac@gmail.com>
2022-02-03 17:32:44 -0300

    finalizando exercícios

--- modulo4/fluxo-de-dados-backend/src/data.ts
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -1,4 +1,4 @@
-type Objects = {
+export type Objects = {
     id: string,
     name: string,
     price: number
@@ -7,27 +7,27 @@ type Objects = {
 export const objects:Objects[] = [
     {
         id: "1",
-        name: "Espelho de couro com alça",
+        name: "Espelho com alça",
         price: 369,
     },
     {
         id: "2",
-        name: "Aparador de grama elétrico",
+        name: "Aparador de Grama",
         price: 160,
     },
     {
         id: "3",
-        name: "Tênis sb Chron 2 canvas",
+        name: "Tênis Chron",
         price: 400,
     },
     {
         id: "4",
-        name: "Cadeirinha infantil para carro",
+        name: "Cadeirinha infantil",
         price: 200,
     },
     {
         id: "5",
-        name: "Máquina de café espresso",
+        name: "Máquina de café",
         price: 450,
     }
 
