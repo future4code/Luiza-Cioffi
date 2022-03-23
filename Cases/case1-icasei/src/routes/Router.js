35cd59eb2f171008f38449e9f9e614194d2de362
Luiza Cioffi <luizagac@gmail.com>
2022-03-23 20:12:06 -0300

    criando páginas, router e coordinator

---
+++ Cases/case1-icasei/src/routes/Router.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter, Switch, Route  } from "react-router-dom";
+import DetailPage from '../pages/DetailPage/DetailPage';
+import HomePage from '../pages/HomePage/HomePage';
+import ResultsPage from '../pages/ResultsPage/ResultsPage';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
+
+
+const Router = () => {
+
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <HomePage/>
+                </Route>
+                <Route exact path="/results">
+                    <ResultsPage/>
+                </Route>
+                <Route exact path="/detail/:id">
+                    <DetailPage/>
+                </Route>
+                <Route>
+                    <ErrorPage/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+
+    )
+}
+
+export default Router
